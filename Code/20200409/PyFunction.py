f637402f2cc88fd4a98185818f5aed14eab61be5
13294 <13294561832@163.com>
2020-04-10 16:16:53 +0800

    模块导入

--- Code/20200409/PyFunction.py
+++ Code/20200409/PyFunction.py
@@ -2,7 +2,8 @@
 日期：2020-4-9 21:27:44
 作者：LF
 邮箱：jksj27250@gmail.com
-主题：python 函数
+主题：
+    1.python 函数
 内容：
     1.函数声明：
         a. 定义函数使用def关键字,例如：
@@ -17,5 +18,13 @@ def add(a,b):
     print("%d + %d = %d\n"%(a,b,c))
     return c
 
-e = add(3,4)
-print(e)
+def main():
+    a = int(input("输入数字："))
+    b = int(input("输入数字："))
+    d =  add(a,b)
+
+if __name__ == '__main__':
+    main()
+    e = add(3,4)
+    print(e)
+
