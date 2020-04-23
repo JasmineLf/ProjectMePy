8b3237b55090509d9b2d5260489d98a343ae25ad
13294 <13294561832@163.com>
2020-04-23 15:47:56 +0800

    python 多线程编程：1.线程创建 2线程同步 2.1互斥锁 2.2条件变量

--- venv/Code/20200420/PyThread.py
+++ venv/Code/20200420/PyThread.py
@@ -9,7 +9,9 @@
         【1】方式一：自定义函数的方式
         【2】方式二：自定义类的方式
     2.多线程编程的类和对象
-    3.线程同步
+    3.线程同步：
+        3.1互斥锁
+        3.2条件变量
 
 '''
 
@@ -118,5 +120,64 @@ for i in range(10):
 [t.join() for t in thread_lst]
 print('num_0 最后的值为：{}'.format(num_0))
 '''
+
+'''
+#条件变量
+#生产者线程
+class ProduceThread(threading.Thread):
+    def __init__(self,name=None,con=None):
+        super().__init__()
+        self.name = name
+        self.con = con
+
+    def run(self):
+        #锁定线程
+        global num
+        self.con.acquire()
+        print('工厂开始生产……')
+        while True:
+            num += 1;
+            print("已生产商品数量：{}\n".format(num))
+            if num >= 5:
+                print("商品数量达到5件，仓库饱满，停止生产...")
+                con.notify()              #唤醒消费者
+                con.wait()                #生产者自身陷入沉睡
+        #释放锁
+        self.con.release()
+
+#消费者线程
+class ConsumerThread(threading.Thread):
+    def __init__(self,name=None,con=None):
+        super().__init__()
+        self.name = name
+        self.con = con
+
+    def run(self):
+        #获取锁
+        con.acquire()
+        global num
+        print("消费者开始消费....")
+        while True:
+            num -= 1
+            print("剩余商品数量：{}\n".format(num))
+            time.sleep(2)
+            if num <= 0:
+                print("库存为0，通知工厂生产......")
+                con.notify()            #唤醒生产线程
+                con.wait()              #消费者自身陷入沉睡
+        #释放锁
+        self.con.release()
+
+if __name__ == '__main__':
+    print("主线程启动")
+    con = threading.Condition()
+    num = 0
+    p = ProduceThread("生产者线程", con)
+    c = ConsumerThread("消费者线程", con)
+    p.start()
+    c.start()
+'''
+
+
 
 
